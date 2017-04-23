4516eb15c0fabe0e3a6e39c3a3d842cbdd133701
laozhao1005 <laozhao1005@gmail.com>
2017-04-23 14:12:28 +0800

    1、图标阴影的实现

--- src/com/kit/imagelib/tools/ImageLoader.java
+++ src/com/kit/imagelib/tools/ImageLoader.java
@@ -1,5 +1,12 @@
 package com.kit.imagelib.tools;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Animatable;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+
 import com.kit.imagelib.interfaces.IImageLoader;
 import com.kit.utils.log.ZogUtils;
 
@@ -20,4 +27,8 @@ public class ImageLoader {
     public static void setImageLoader(IImageLoader yourImageLoader) {
         iImageLoader = yourImageLoader;
     }
+
+
+
+
 }
